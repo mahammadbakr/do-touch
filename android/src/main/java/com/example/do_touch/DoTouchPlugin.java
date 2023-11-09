4fe07607182031b42bf5513f1daa5789abbcc02c
Mahammad Bakr <49376835+mahammadbakr@users.noreply.github.com>
2023-11-10 01:52:12 +0300

    Update DoTouchPlugin.java

--- android/src/main/java/com/example/do_touch/DoTouchPlugin.java
+++ android/src/main/java/com/example/do_touch/DoTouchPlugin.java
@@ -51,8 +51,7 @@ public class DoTouchPlugin implements FlutterPlugin, MethodCallHandler {
             double y = call.argument("y");
             boolean canSwipe = call.argument("canSwipe");
 
-            TouchHandler.tap(x, y);
-            // touchPoint((float) x, (float) y, canSwipe);
+            touchPoint((float) x, (float) y, canSwipe);
             result.success(true);
         } else if (call.method.equals("isDeviceLocked")) {
             KeyguardManager myKM = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
